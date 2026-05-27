f2ca0f48fff4d9a6eb1117be7bde201220c493d6
Sornapriyadas <sornapriyadas@gmail.com>
2026-05-27 10:40:35 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll(".product-card button");
+
+buttons.forEach(button => {
+
+  button.addEventListener("click", () => {
+
+    alert("Product Added To Cart ❤️");
+
+  });
+
+});
